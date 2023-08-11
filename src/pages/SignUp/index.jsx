99c4970f1a704e5583de16bd35c25667b7ecdca1
daiane <daiane.ajmachado@gmail.com>
2023-08-11 17:30:47 -0300

    feat: create signup page mobile first

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,9 +1,45 @@
-import { Container } from "./styles";
+import logoIcon from "../../assets/logo.svg";
+
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+
+import { Container, BrandWrapper, Form } from "./styles";
 
 export default function SignUp() {
   return (
     <Container>
-      <h1>Hello world</h1>
+      <BrandWrapper>
+        <img src={logoIcon} alt="Logo" />
+        <h1>food explorer</h1>
+      </BrandWrapper>
+
+      <Form>
+        <Input 
+          label="Seu nome" 
+          type="text" 
+          placeholder="Exemplo: Maria da Silva"
+          id="userName"
+        />
+
+        <Input 
+          label="Email" 
+          type="userEmail" 
+          placeholder="Exemplo: exemplo@email.com"
+          id="userEmail"
+        />
+
+        <Input 
+          label="Senha" 
+          type="userPassword" 
+          placeholder="No mínimo 6 caracteres"
+          id="userPassword"
+        />
+
+        <Button title="Criar conta" />
+
+        <a href="#">Já tenho uma conta</a>
+      </Form>
+
     </Container>
   );
 }
--- src/pages/SignUp/styles.js
+++ src/pages/SignUp/styles.js
@@ -1,5 +1,41 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  background: ${({ theme }) => theme.COLORS.GRADIENTS_100};
+  padding-inline: 4.7rem;
+  padding-inline: 2rem;
+  min-height: 100vh;
+
+  display: grid;
+  align-content: center;
+`;
+
+export const BrandWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+  margin-top: 2rem;
+
+  > h1 {
+    font-size: 3.7rem;
+  }
+
+  > img {
+    width: 4.3rem;
+    height: 4.3rem;
+  }
+`;
+
+export const Form = styled.form`
+  margin: 7.3rem 0 3.2rem 0;
+  text-align: center;
+
+  > div, button {
+    margin-bottom: 3.2rem;
+  }
+
+  > a {
+    color: ${({ theme }) => theme.COLORS.LIGHT_100};
+  }
 `;
