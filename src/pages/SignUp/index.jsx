d0cda1e5c7834d542a4f2d652d0f027769ef5f4a
daiane <daiane.ajmachado@gmail.com>
2023-10-06 16:09:31 -0300

    feat: create a new user

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,3 +1,7 @@
+import { useState } from "react";
+import { api } from "../../services/api";
+import { useNavigate } from "react-router-dom";
+
 import { Logo } from "../../components/Logo";
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
@@ -5,6 +9,31 @@ import { Button } from "../../components/Button";
 import { Container, Form } from "./styles";
 
 export function SignUp() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  function handleSignUp() {
+    if (!name || !email || !password) {
+      return alert("Preencha todos os campos.");
+    };
+
+    api.post("/users", { name, email, password })
+      .then(() => {
+        alert("Usuário cadastrado com sucesso.");
+        navigate("/");
+      })
+      .catch(error => {
+        if (error.response) {
+          alert(error.response.data.message);
+        } else {
+          alert("Não foi possível cadastrar este usuário.");
+        };
+      })
+  }
+
   return (
     <Container>
 
@@ -18,6 +47,7 @@ export function SignUp() {
           type="text" 
           placeholder="Exemplo: Maria da Silva"
           id="userName"
+          onChange={e => setName(e.target.value)}
         />
 
         <Input 
@@ -25,6 +55,7 @@ export function SignUp() {
           type="email" 
           placeholder="Exemplo: exemplo@email.com"
           id="userEmail"
+          onChange={e => setEmail(e.target.value)}
         />
 
         <Input 
@@ -32,9 +63,10 @@ export function SignUp() {
           type="password" 
           placeholder="No mínimo 6 caracteres"
           id="userPassword"
+          onChange={e => setPassword(e.target.value)}
         />
 
-        <Button title="Criar conta" />
+        <Button title="Criar conta" onClick={handleSignUp} />
 
         <Button variant="secondary" title="Já tenho uma conta" fontSize="1.4rem"/>
       </Form>
