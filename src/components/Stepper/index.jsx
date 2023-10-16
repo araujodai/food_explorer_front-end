58c67d8d8eac446f891ad456e83abd0ff4c27c78
daiane <daiane.ajmachado@gmail.com>
2023-10-16 15:24:43 -0300

    feat: controls number os items in the cart

--- src/components/Stepper/index.jsx
+++ src/components/Stepper/index.jsx
@@ -1,14 +1,34 @@
+import { useState } from "react";
+import { useCart } from "../../hooks/cart";
+
 import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 
 import { Button } from "../Button";
+
 import { Container } from "./styles";
 
 export function Stepper() {
+  const { amount, handleAddUnit, handleRemoveUnit } = useCart();
+
   return (
     <Container>
-      <Button variant="secondary" icon={AiOutlineMinus} size="24px" />
-      <span>01</span>
-      <Button variant="secondary" icon={AiOutlinePlus} size="24px" />
+      <Button 
+        variant="secondary" 
+        icon={AiOutlineMinus} 
+        size="24px" 
+        onClick={handleRemoveUnit}
+      />
+
+      <span>
+        {String(amount).padStart(2, "0")}
+      </span>
+
+      <Button 
+        variant="secondary" 
+        icon={AiOutlinePlus} 
+        size="24px" 
+        onClick={handleAddUnit}
+      />
     </Container>
   )
 };
