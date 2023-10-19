9944f7ee26188d056aedaab7083d70b541c36a40
daiane <daiane.ajmachado@gmail.com>
2023-10-19 20:08:35 -0300

    feat: create a new dish

--- src/pages/NewDish/index.jsx
+++ src/pages/NewDish/index.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { MdKeyboardArrowLeft } from "react-icons/md";
 
 import { Header } from "../../components/Header";
@@ -9,23 +11,82 @@ import { IngredientTag } from "../../components/IngredientTag";
 import { Textarea } from "../../components/Textarea";
 import { Footer } from "../../components/Footer";
 
+import { api } from "../../services/api";
+
 import { Container, ContentWrapper, Form, IngredientGroup } from "./styles";
 
 export function NewDish() {
+  const [ imageUploaded, setImageUploaded ] = useState(null);
+  const [ name, setName ] = useState("");
+  const [ category, setCategory ] = useState("");
+
+  const [ ingredients, setIngredients ] = useState([]);
+  const [ newIngredient, setNewIngredient ] = useState("");
+
+  const [ price, setPrice ] = useState("");
+  const [ description, setDescription ] = useState("");
+
+  const navigate = useNavigate();
+
+  function handleBack() {
+    navigate(-1)
+  };
+
+  function handleAddIngredient() {
+    setIngredients(prevState => [...prevState, newIngredient]);
+    setNewIngredient("");
+  };
+
+  function handleRemoveIngredient(deleted) {
+    setIngredients(prevState => prevState.filter(ingredient => ingredient !== deleted));
+  };
+
+  async function handleNewMenuItem() {
+    try {
+      const menuItem = new FormData();
+  
+      menuItem.append("image", imageUploaded);
+      menuItem.append("name", name);
+      menuItem.append("category", category);
+      menuItem.append("ingredients", JSON.stringify(ingredients));
+      menuItem.append("price", price);
+      menuItem.append("description", description);
+  
+      await api.post("/menu", menuItem);
+  
+      alert("Item criado com sucesso!");
+      navigate(-1);
+
+    } catch (error) {
+      if (error.response) {
+        alert(error.response.data.message);
+
+      } else {
+        alert("Não foi possível criar esse item, verifique e tente novamente.");
+      };
+    };
+  };
+
   return (
     <Container>
       <Header />
 
       <ContentWrapper>
         <main className="contentMaxWidthWrapper">
-          <Button variant="secondary" title="voltar" icon={MdKeyboardArrowLeft} />
+          <Button 
+            variant="secondary" 
+            title="voltar" 
+            icon={MdKeyboardArrowLeft} 
+            onClick={handleBack}
+          />
 
           <Form>
             <h1>Novo prato</h1>
 
             <InputFile 
               id="mealImage"
-              title="Selecione imagem"
+              title="Adicionar imagem"
+              onChange={e => setImageUploaded(e.target.files[0])}
             />
 
             <Input 
@@ -33,31 +94,33 @@ export function NewDish() {
               type="text" 
               placeholder="Ex.: Salada Ceasar"
               id="dishName"
+              onChange={e => setName(e.target.value)}
             />
             
             <SelectCustom
               title="Categoria"
+              onChange={e => setCategory(e.target.value)}
             />
 
             <IngredientGroup>
               <strong>Ingredientes</strong>
               
               <div className="tagsWrapper">
-
-                <IngredientTag 
-                  value="Adicionar"
-                />
-
-                <IngredientTag 
-                  value="ovo frito"
-                />
-
-                <IngredientTag 
-                  value="arroz"
-                />
+                {
+                  ingredients.map((ingredient, index) => (
+                    <IngredientTag 
+                      key={String(index)}
+                      value={ingredient}
+                      onClick={() => handleRemoveIngredient(ingredient)}
+                    />
+                  ))
+                }
 
                 <IngredientTag 
                   isNew
+                  value={newIngredient}
+                  onChange={e => setNewIngredient(e.target.value)}
+                  onClick={handleAddIngredient}
                 />
               </div>
             </IngredientGroup>
@@ -67,17 +130,20 @@ export function NewDish() {
               type="text" 
               placeholder="R$ 00,00"
               id="dishPrice"
+              onChange={e => setPrice(e.target.value)}
             />
 
             <Textarea 
               title="Descrição"
               id="dishDescription"
               placeholder="Fale brevemente sobre o prato, seus ingredientes e composição"
+              onChange={e => setDescription(e.target.value)}
             />
 
             <Button 
               title="Salvar alterações"
               className="light"
+              onClick={handleNewMenuItem}
             />
 
           </Form>
