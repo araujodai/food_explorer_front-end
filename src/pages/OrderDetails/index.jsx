802e2dfbea3581e4e725fdecd577a703566558f8
daiane <daiane.ajmachado@gmail.com>
2024-03-14 16:23:13 -0300

    feat: creates order details page

---
+++ src/pages/OrderDetails/index.jsx
@@ -0,0 +1,132 @@
+import { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
+import { api } from "../../services/api";
+import { status } from "../../components/SelectCustom";
+
+import { MdOutlineWatchLater, MdCheckCircleOutline, MdNoMeals } from "react-icons/md";
+import { PiForkKnife } from "react-icons/pi";
+
+import { MdKeyboardArrowLeft } from "react-icons/md";
+import imagePlaceholder from "../../assets/menu_item_image_placeholder.png";
+
+import { Header } from "../../components/Header";
+import { Button } from "../../components/Button";
+import { SimpleCard } from "../../components/SimpleCard";
+import { PaymentSelection } from "../../components/PaymentSelection";
+import { Footer } from "../../components/Footer";
+
+import { notify } from "../../components/Notification";
+import { Loading } from "../../components/Loading";
+
+import { Container, ContentWrapper } from "./styles";
+
+export function OrderDetails() {
+  const [ data, setData ] = useState(null);
+  const [ showPaymentSection, setShowPaymentSection ] = useState(false);
+  const [ isLoading, setIsLoading ] = useState(false);
+
+  const navigate = useNavigate();
+  const params = useParams();
+
+  function handleSelectPaymentMethod() {
+    setShowPaymentSection(prevState => !prevState)
+  };
+
+  function renderOrderStatus(status, situation) {
+    const foundItem = situation.find(item => item.value === status);
+
+    const IconMap = {
+      pending: <MdOutlineWatchLater />,
+      preparing: <MdCheckCircleOutline />,
+      delivered: <PiForkKnife />,
+      canceled: <MdNoMeals />,
+    }
+
+    if (foundItem) {
+      return (
+        <>
+          {IconMap[foundItem.value]}
+          <p>{foundItem.description}</p>
+        </>
+      )
+    }
+  };
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    async function fetchOrderDetails() {
+      try {
+        const response = await api.get(`/orders/${params.id}`)
+        setData(response.data);
+        setIsLoading(false);
+        // console.log(response.data);
+
+      } catch (error) {
+        if (error.response) {
+          notify.error(error.response.data.message);
+
+        } else {
+          notify.error("Não foi possível carregar as informações desse pedido, tente novamente.");
+        };
+      };
+    };
+
+    fetchOrderDetails()
+  }, []);
+
+  return (
+    <Container>
+      <Header />
+
+      <ContentWrapper>
+        <main className="contentMaxWidthWrapper">
+          {/* {isLoading && <Loading /> } */}
+          <Loading isVisible={isLoading} />
+
+          {data && (
+            <>
+              <section className={showPaymentSection ? "hide" : ""}>
+                <h1>Meu pedido</h1>
+
+                <div>
+                  {data.order_items && 
+                    data.order_items.map(item => (
+                      <SimpleCard
+                        key={String(item.id)}
+                        data={item}
+                      />
+                    ))
+                  }
+
+                  <span>Total: R$ {data.price.toLocaleString('pt-BR', { minimumFractionDigits: 2 })}</span>
+                </div>
+
+                <Button title="Status" onClick={handleSelectPaymentMethod}/>
+              </section>
+
+              <section className={!showPaymentSection ? "hide" : ""}>
+                <Button 
+                  variant="secondary" 
+                  title="voltar" 
+                  icon={MdKeyboardArrowLeft} 
+                  onClick={handleSelectPaymentMethod} 
+                />
+
+                <h1>Pagamento</h1>
+
+                <PaymentSelection>
+                  <div className="orderStatus">
+                    {renderOrderStatus(data.status, status)}
+                  </div>
+                </PaymentSelection>
+              </section>
+            </>
+          )}
+        </main>
+        <Footer />
+      </ContentWrapper>
+    </Container>
+  );
+};
---
+++ src/pages/OrderDetails/styles.js
@@ -0,0 +1,139 @@
+import styled, { keyframes } from "styled-components";
+
+const slideInRight = keyframes`
+  from {
+    transform: translateX(100%);
+    opacity: 0;
+  }
+  to {
+    transform: translateX(0);
+    opacity: 1;
+  }
+`;
+
+const slideInLeft = keyframes`
+  from {
+    transform: translateX(-100%);
+    opacity: 0;
+  }
+  to {
+    transform: translateX(0);
+    opacity: 1;
+  }
+`;
+
+export const Container = styled.div`
+  width: 100%;
+  width: 100vw;
+  height: 100vh;
+  display: grid;
+  grid-template-rows: min-content auto;
+  grid-template-areas: 
+  "header"
+  "ContentWrapper";
+  overflow: hidden;
+
+  > header {
+    grid-area: header;
+  }  
+`;
+
+export const ContentWrapper = styled.div`
+  grid-area: ContentWrapper;
+  display: grid;
+  grid-template-rows: auto min-content;                                          
+  grid-template-areas: 
+  "main"
+  "footer";
+  overflow-x: hidden;
+  overflow-y: auto;
+
+  > main {
+    grid-area: main;
+    padding: 3.4rem 2rem;
+
+    > section {
+      display: grid;
+      grid-template-rows: min-content auto min-content min-content;
+     
+      h1 {
+        font: 500 3.2rem/140% "Poppins", sans-serif;
+        margin-bottom: 2.7rem;
+      }
+
+      &.hide {
+        display: none;
+      }
+
+      &:first-of-type {
+        animation: ${slideInLeft} 0.5s forwards;
+        height: 100%;
+
+        > div > span {
+          display: block;
+          margin: 3.2rem 0 4.7rem 0;
+          font: 500 2rem/160% "Poppins", sans-serif;
+        }
+
+        > button {
+          width: 21rem;
+          margin: 0 0 0 auto;
+          height: auto;
+        }
+      }
+
+      &:last-of-type {
+        animation: ${slideInRight} 0.5s forwards;
+
+        .orderStatus {
+          text-align: center;
+          color: ${({ theme }) => theme.COLORS.LIGHT_400};
+          font-size: 2rem;
+          font-weight: 700;
+          line-height: 160%;
+
+          svg {
+            width: 25%;
+            height: auto;
+            margin-bottom: 2.4rem;
+          }
+        }
+      }
+    }
+
+    @media (min-width: 1024px) {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      grid-column-gap: 5%;
+
+      > section {
+        h1 {
+          margin-bottom: 3.2rem;
+        }
+
+        &.hide {
+          display: initial;
+        }
+
+        &:first-of-type {
+          animation: none;
+
+          > button {
+            display: none;
+          }
+        }
+
+        &:last-of-type > button {
+          display: none;
+          /* > button {
+            display: none;
+          } */
+        }
+      }
+    }
+  }
+
+  > footer {
+    grid-area: footer;
+  }
+`;
